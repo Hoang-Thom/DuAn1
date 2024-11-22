4de7053b7ed699a9628cfa5cb2464cb0113f6177
Binh Nguyen <binhnhps40254@gmail.com>
2024-11-22 18:25:59 +0700

    Binh them case blog

--- site/index.php
+++ site/index.php
@@ -13,6 +13,9 @@
         case 'cart':
             echo "Trang cart";
             break;
+        case 'blog':
+            echo "Trang bài viết";
+            break;
         case 'login':
             echo "Trang Login";
             break;
