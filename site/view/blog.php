373b678cb3ea7dc20eed5ffc72bf7f198ae29627
Binh Nguyen <binhnhps40254@gmail.com>
2024-11-22 19:46:16 +0700

    binh add blog

---
+++ site/view/blog.php
@@ -0,0 +1,48 @@
+<section class="blog-posts">
+            <h2>Bài viết</h2>
+            <div class="post-list">
+                <div class="post">
+                    <a href="../html/blog.html">
+                        <img src="../public/img/daylambanhkem.jpg" alt="Blog Post Image">
+                        <h3>Các buổi workshop làm bánh được tổ chức định kỳ, thu hút sự tham gia của nhiều người yêu thích ẩm thực.</h3>
+                        <div class="post-meta">
+                            <p>Viết bởi chúng tôi</p>
+                            <p class="date">02/11/2024</p>
+                        </div>
+                    </a>
+                </div>
+                
+                <div class="post">
+                    <a href="../html/blog.html">
+                        <img src="../public/img/phanbietcacloaibanh.jpeg" alt="Blog Post Image">
+                        <h3>Khi nói đến các loại bánh, có rất nhiều loại khác nhau với hương vị và cách chế biến riêng. Dưới đây là cách phân biệt một số loại bánh phổ biến mà bạn có thể gặp.</h3>
+                        <div class="post-meta">
+                            <p>Viết bởi chúng tôi</p>
+                            <p class="date">01/11/2024</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="post">
+                    <a href="../html/blog.html">
+                        <img src="../public/img/quytrinhlambanh.jpg" alt="Blog Post Image">
+                        <h3>Làm bánh kem không chỉ đơn thuần là một nghệ thuật, mà còn là một khoa học. Dưới đây là quy trình chi tiết mà chúng tôi thực hiện để tạo ra những chiếc bánh kem thơm ngon và đẹp mắt.</h3>
+                        <div class="post-meta">
+                            <p>Viết bởi chúng tôi</p>
+                            <p class="date">30/10/2024</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="post">
+                    <a href="../html/blog.html">
+                        <img src="../public/img/welcome.jpg" alt="Blog Post Image">
+                        <h3>Chúng tôi, những người sáng lập, luôn tin rằng bánh kem không chỉ là món ăn mà còn là một tác phẩm nghệ thuật, mang đến niềm vui và hạnh phúc cho mọi người.</h3>
+                        <div class="post-meta">
+                            <p>Viết bởi chúng tôi</p>
+                            <p class="date">29/10/2024</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
