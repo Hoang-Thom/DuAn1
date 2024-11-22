395fe9902f149be17b6e50ceade7eb9a56af5929
Binh Nguyen <binhnhps40254@gmail.com>
2024-11-22 20:52:57 +0700

    binh add blog

--- site/view/header.php
+++ site/view/header.php
@@ -6,6 +6,7 @@
     <title>SweetCakes</title>
     <link rel="stylesheet" href="../public/css/site/home.css">
     <link rel="stylesheet" href="../public/css/site/login.css">
+    <link rel="stylesheet" href="../public/css/site/blog.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
