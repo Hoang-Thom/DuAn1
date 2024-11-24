1e473be12e981eea7ea50e85c45e54baeb3c3926
Thom Hoang <thomhnps36607@fpt.edu.vn>
2024-11-24 14:22:25 +0700

    Thom fix shop detail

--- site/view/detail.php
+++ site/view/detail.php
@@ -1,14 +1,14 @@
     <main>
-        <div class="container">
+        <div class="container-detail">
             <div class="product">
                 <div class="pro-img">
                     <div class="pro-img-big">
-                        <img id="mainImage" src="../img/pie&tart-detail1.jpg" alt>
+                        <img id="mainImage" src="../public/img/pie-1.png" alt>
                     </div>
                     <div class="pro-img-mini">
-                        <img onclick="changeImage(this)" src="/img/pie&tart-detail2.jpg" alt>
-                        <img onclick="changeImage(this)" src="/img/pie&tart-detail3.jpg" alt>
-                        <img onclick="changeImage(this)" src="/img/pie&tart-detail1.jpg" alt>
+                        <img onclick="changeImage(this)" src="../public/img/pie&tart-01.jpg" alt>
+                        <img onclick="changeImage(this)" src="../public/img/pie&tart-01.jpg" alt>
+                        <img onclick="changeImage(this)" src="../public/img/pie&tart-01.jpg" alt>
                     </div>
                 </div>
 
@@ -52,7 +52,7 @@
                 </div>
                 <div class="danhgia-content">
                     <div class="danhgia-name">
-                        <img src="../img/tiramisu-10.jpg" alt="">
+                        <img src="../public/img/tiramisu-10.jpg" alt="">
                         <h3>Tên khách hàng đánh giá</h3>
                     </div>
                     <div class="star">
@@ -64,14 +64,14 @@
                     </div>
                     <h4>Bánh ngon trang trí đẹp mắt giao hàng nhanh chóng bảo quản bánh tốt, khi giao đến bánh vẫn đẹp.</h4>
                     <div class="danhgia-ims">
-                        <img src="/img/pie&tart-detail1.jpg" alt>
-                        <img src="/img/pie&tart-detail2.jpg" alt>
+                        <img src="../public/img/pie&tart-04.jpg" alt>
+                        <img src="../public/img/pie&tart-05.jpg" alt>
                     </div>
                 </div>
 
                 <div class="danhgia-content">
                     <div class="danhgia-name">
-                        <img src="../img/tiramisu-10.jpg" alt="">
+                        <img src="../public/img/tiramisu-10.jpg" alt="">
                         <h3>Tên khách hàng đánh giá</h3>
                     </div>
                     <div class="star">
@@ -83,37 +83,37 @@
                     </div>
                     <h4>Bánh ngon trang trí đẹp mắt giao hàng nhanh chóng bảo quản bánh tốt, khi giao đến bánh vẫn đẹp.</h4>
                     <div class="danhgia-ims">
-                        <img src="/img/pie&tart-detail3.jpg" alt>
-                        <img src="/img/pie&tart-detail4.jpg" alt>
+                        <img src="../public/img/pie&tart-01.jpg" alt>
+                        <img src="../public/img/pie&tart-02.jpg" alt>
                     </div>
                 </div>
             </div>
 
-            <div class="underline-both-1">Sản phẩm tương tự</div>
+            <div class="underline">Sản phẩm tương tự</div>
             <div class="cakes">
                 <div class="meu-cake">
                     <button class="left-1"><i class="fa-solid fa-angle-left"></i></button>
                     <div class="cake-4">
                         <div class="col-4-1 mgr-10">
-                            <div class="img-cakes"><img src="../img/pie-1.png" alt=""></div>
+                            <div class="img-cakes"><img src="../public/img/pie-1.png" alt=""></div>
                             <div class="price-cake">120 000 VNĐ</div>
                             <div class="name-cake">Tart mini fruits</div>
                             <button>Thêm vào giỏ hàng</button>
                         </div>
                         <div class="col-4-1 mgr-10">
-                            <div class="img-cakes"><img src="../img/pie-2.png" alt=""></div>
+                            <div class="img-cakes"><img src="../public/img/pie-2.png" alt=""></div>
                             <div class="price-cake">20 000 VNĐ</div>
                             <div class="name-cake">Pie mini mix</div>
                             <button>Thêm vào giỏ hàng</button>
                         </div>
                         <div class="col-4-1 mgr-10">
-                            <div class="img-cakes"><img src="../img/pie-3.png" alt=""></div>
+                            <div class="img-cakes"><img src="../public/img/pie-3.png" alt=""></div>
                             <div class="price-cake">500 000 VNĐ</div>
                             <div class="name-cake">Tart strawberry</div>
                             <button>Thêm vào giỏ hàng</button>
                         </div>
                         <div class="col-4-1">
-                            <div class="img-cakes"><img src="../img/pie-4.png" alt=""></div>
+                            <div class="img-cakes"><img src="../public/img/pie-4.png" alt=""></div>
                             <div class="price-cake">50 000 VNĐ</div>
                             <div class="name-cake">Tart mango mix blueberry</div>
                             <button>Thêm vào giỏ hàng</button>
